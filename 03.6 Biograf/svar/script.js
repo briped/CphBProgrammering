32e1550b4ac361f17b058d3b969d6d5a41994c7a
briped <briped@live.com>
2018-10-26 18:24:13 +0200

    svar

--- 03.6 Biograf/svar/script.js
+++ 03.6 Biograf/svar/script.js
@@ -1,41 +1,47 @@
-//var cinema = []; // Declare global array.
+let cinema = []; // Declare global array.
 
-function doCreate() {
-    let cinema = [];
+function createCinema() {
+    //let cinema = [];
     let rows = Number(document.getElementById('rows').value);
     let seats = Number(document.getElementById('seats').value)
-    console.log('rows: ' + rows);
-    console.log('seats: ' + seats);
+    let tableRows = '';
     for (let r = 0; r < rows; r++) {
         cinema.push([]); // Create the 'row'.
+        let tableCols = '';
         for (let c = 0; c < seats; c++) {
             cinema[r].push(true); // Add a seat/column to the current row.
-        }
-    }
-    //console.log(cinema);
-    return cinema;
-}
-
-function drawRoom(cinema) {
-    let tableRows = '';
-    for (let r = 0; r < cinema.length; r++) {
-        let tableCols = '';
-        for (let c = 0; c < cinema[r].length; c++) {
-            /*
-            if (r === 0) {
-                tableCols += (c === 0) ? '<td></td>' : `<td>c${c}</td>`;
-            }
-            else {
-                tableCols += (c === 0) ? `<td>r${r}</td>` : `<td><button id="r${r}c${c}" onclick="doBooking(this); return false;">_</button></td>`;
-            }
-            */
-            tableCols += `<td><button id="r${r}c${c}" style="background-color: green;" onclick="doBooking(this); return false;">${r}.${c}</button></td>`;
+            tableCols += `<td><button id="r${r}c${c}" style="background-color: green;" onclick="bookSeat(this); return false;">${r}.${c}</button></td>`;
         }
         tableRows += '<tr>' + tableCols + '</tr>';
     }
     document.getElementById('room').innerHTML = tableRows;
+    document.getElementById('availabilityUI').style.visibility = 'visible';
 }
 
-function doBooking(element) {
+function bookSeat(element) {
+    let seatCoordinates = element.innerText.split('.');
+    let seatRow = seatCoordinates[0];
+    let seatCol = seatCoordinates[1];
+    cinema[seatRow][seatCol] = (cinema[seatRow][seatCol] === true) ? false : true;
     element.style.backgroundColor = (element.style.backgroundColor == 'red') ? 'green' : 'red';
+}
+
+function listSeats(state) {
+    let output = '';
+    let seatsFound = 0;
+    for (let r = 0; r < cinema.length; r++) {
+        for (let c = 0; c < cinema[r].length; c++) {
+            if (cinema[r][c] === true && (state =='all' || state == 'available')) {
+                output += (seatsFound > 0) ? ', ' : '';
+                output +=  `<span style="color: green;">${r}.${c}</span>`;
+                seatsFound++;
+            }
+            if (cinema[r][c] === false && (state =='all' || state == 'unavailable')) {
+                output += (seatsFound > 0) ? ', ' : '';
+                output +=  `<span style="color: red;">${r}.${c}</span>`;
+                seatsFound++;
+            }
+        }
+    }
+    document.getElementById('seatList').innerHTML = output;
 }
